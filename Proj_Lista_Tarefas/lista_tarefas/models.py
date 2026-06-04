d8c0b750db9dc2a574fdef5ef1a5b2746d2e023d
gogoncalves <gogoncalves@gmail.com>
2026-06-04 18:07:28 -0300

    A15 - Atualizacao de tab no BD após ja estar populada

--- Proj_Lista_Tarefas/lista_tarefas/models.py
+++ Proj_Lista_Tarefas/lista_tarefas/models.py
@@ -1,16 +1,19 @@
 from django.db import models
+# Importo o "User" de ambiente de controle de auth
+# para conectar ele a tab Topicos onde cada topico
+# terá uma coluna de dono "owner"
+from django.contrib.auth.models import User
 
-# Create your models here.
 
-# Nova tabela para lista de tarefas principais
 class Topicos(models.Model):
     """Titulos das tarefas"""
-    # Cria campo texto com tamanho de 200 caracteres
     text = models.CharField(max_length=200)
-    # Função para preencher automaticamente data do registro
     date_added = models.DateTimeField(auto_now=True)
-
-    # Função para ajustar o como será mostrado no BD as infos no painel ADM
+    # Definicao de um vinculo dos registro na tab topicos
+    # com o id que é dono dela. Quando faço a modificação em
+    # uma tab que já está populada, eu preciso fazer o
+    # "makemigrations" e o "migrate".
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """ Mostra o como será mostrado os dados no ADM"""
@@ -21,8 +24,8 @@ class Topicos(models.Model):
 class Sub_topicos(models.Model):
     """ Estrutura de subtopicos dentro de uma tarefa """
 
-    # Esta funcao serve para ligar a PK de Topicos e se ela for deletada, ele deleta
-    # todos os subtopicos da tabela ligada a ela.
+    # Esta funcao serve para ligar a PK de Topicos e se ela for deletada,
+    # ele deleta todos os subtopicos da tabela ligada a ela.
     sub_topicos = models.ForeignKey(Topicos, on_delete=models.CASCADE)
 
     # Função para definir que o campo é para texto longo.
@@ -31,8 +34,8 @@ class Sub_topicos(models.Model):
     # Função para preencher automaticamente data do registro quando atualizado.
     date_added = models.DateTimeField(auto_now_add=True)
 
-
-    # Class especial do Django para tratar nas as palavras em plural na tab adm.
+    # Class especial do Django para tratar nas as palavras em plural
+    # na tab adm.
     class Meta:
         verbose_name_plural = 'sub_topicos_s'
 
