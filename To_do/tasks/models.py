2ca205dd8b05f752348e37a3d8fca7c023037287
gogoncalves <gogoncalves@gmail.com>
2026-07-13 18:57:56 -0300

    A04 - Models e Migrate

--- To_do/tasks/models.py
+++ To_do/tasks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    # Variavel em tupla para o select do done, ele só aceita str
+    STATUS = (
+        ('doing', 'DOING'),
+        ('done', 'DONE'),
+    )
+
+    title = models.CharField(max_length=255)
+    # este não possui comprimento maximo
+    description = models.TextField()
+
+    # Choices é um select do proprio django, ao qual estou passando uma tupla
+    done = models.CharField(
+        max_length=5,
+        choices=STATUS,
+    )
+    # Cria data automaticamente quando o registro é criado
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Cria data automaticamente quando o registro é atualizado
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
